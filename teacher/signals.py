f03d592fdbb1b9905f2e02412e460791a29f748e
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-07-13 20:16:36 +0545

    update user model

--- teacher/signals.py
+++ teacher/signals.py
@@ -1,25 +1,25 @@
-# In teacher/signals.py
+# # In teacher/signals.py
 
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.contrib.auth.models import Group
-from .models import Teacher
-from django.contrib.auth import get_user_model
+# from django.db.models.signals import pre_save
+# from django.dispatch import receiver
+# from django.contrib.auth.models import Group
+# from .models import Teacher
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
-@receiver(pre_save, sender=Teacher)
-def create_user_for_teacher(sender, instance, **kwargs):
-    if not instance.user_id:
-        user = User.objects.create_user(
-            username=instance.unique_username,
-            password=instance.dob.strftime('%Y-%m-%d'),
-            email=instance.email,
-            first_name=instance.first_name,
-            last_name=instance.last_name
-        )
-        instance.user = user
+# @receiver(pre_save, sender=Teacher)
+# def create_user_for_teacher(sender, instance, **kwargs):
+#     if not instance.user_id:
+#         user = User.objects.create_user(
+#             username=instance.unique_username,
+#             password=instance.dob.strftime('%Y-%m-%d'),
+#             email=instance.email,
+#             first_name=instance.first_name,
+#             last_name=instance.last_name
+#         )
+#         instance.user = user
 
-        # Add user to 'teacher' group
-        teacher_group, created = Group.objects.get_or_create(name='Teacher')
-        teacher_group.user_set.add(user)
+#         # Add user to 'teacher' group
+#         teacher_group, created = Group.objects.get_or_create(name='Teacher')
+#         teacher_group.user_set.add(user)
