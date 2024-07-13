1b4c94740c70bfd1db0efc75e09cfd4a26a1b5c7
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-07-13 22:28:41 +0545

    update admin

--- enrollmentcrown/urls.py
+++ enrollmentcrown/urls.py
@@ -19,6 +19,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Hello Crown"
+admin.site.site_title = "Ease Enroll"
+admin.site.index_title = "Welcome to Your Enrollment Process"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
