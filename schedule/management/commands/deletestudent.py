9aafef5360c18bed6d39c40ea192144dfbff2418
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-08-03 17:40:29 +0545

    update deletestudent

---
+++ schedule/management/commands/deletestudent.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from student.models import Student
+
+class Command(BaseCommand):
+    help = 'Calculate and store potential subjects along with the number of students remaining to study them'
+
+    def handle(self, *args, **kwargs):
+
+        subjects = Student.objects.all()
+
+        #delete each instance of the model
+
+        for subject in subjects:
+            subject.delete()
+        self.stdout.write(self.style.SUCCESS('Successfully deleted'))
